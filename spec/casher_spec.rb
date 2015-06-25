89f67a7c98041a0c66253336170837ce0efd0888
Hiro Asari <asari.ruby@gmail.com>
2015-06-25 19:37:22 -0400

    Adjust expectation of cache URL
    Real cache URL looks like 'https://example.com/afdfad/master/cache--rvm-default--gemfile-Gemfile.tbz?param1=value1&param2=value2'.
    This is quite different from the previous expectation, where it was
    assumed to *end with* '.tgz'.
    
    Note that we need to hack the specs a little, since
    `/#{url}/` does not escape `?` correctly; specs will fail
    otherwise.

--- spec/casher_spec.rb
+++ spec/casher_spec.rb
@@ -1,24 +1,24 @@
 load File.join(File.dirname(__FILE__), '..', 'bin', 'casher')
 
 describe Casher do
-  let(:tbz_url) { 'master/cache--rvm-default--gemfile-Gemfile.tbz' }
-  let(:tgz_url) { 'master/cache--rvm-default--gemfile-Gemfile.tgz' }
+  let(:tbz_url) { 'https://example.com/afdfad/master/cache--rvm-default--gemfile-Gemfile.tbz?param1=value1&param2=value2' }
+  let(:tgz_url) { 'https://example.com/afdfad/master/cache--rvm-default--gemfile-Gemfile.tgz?param1=value1&param2=value2' }
   subject { described_class.new() }
 
   describe '#run' do
     it 'calls "curl" to download .tbz archive' do
-      expect(subject).to receive(:system).with(/curl\b.*#{tgz_url}\b/m).and_return(true)
+      expect(subject).to receive(:system).with(/curl\b.*#{tgz_url.gsub('?','\?')}/m).and_return(true)
       subject.run('fetch', tbz_url)
     end
   end
 
   context 'when .gz archive is not available' do
     before :each do
-      expect(subject).to receive(:system).with(/curl\b.*#{tgz_url}\b/m).and_return(false)
+      expect(subject).to receive(:system).with(/curl\b.*#{tgz_url.gsub('?','\?')}/m).and_return(false)
     end
 
     it 'falls back to .bz2' do
-      expect(subject).to receive(:system).with(/curl\b.*#{tbz_url}\b/m)
+      expect(subject).to receive(:system).with(/curl\b.*#{tbz_url.gsub('?','\?')}/m)
       subject.run('fetch', tbz_url)
     end
   end
