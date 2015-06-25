6d3ec9e8a3e4a75d997a1b6757cd780c91e1ec11
Hiro Asari <asari.ruby@gmail.com>
2015-06-25 16:28:18 -0400

    Start specs

--- spec/casher_spec.rb
+++ spec/casher_spec.rb
@@ -1,7 +1,14 @@
-describe 'casher' do
-  before do
-  end
+load File.join(File.dirname(__FILE__), '..', 'bin', 'casher')
 
-  after do
+describe Casher do
+  let(:tbz_url) { 'master/cache--rvm-default--gemfile-Gemfile.tbz' }
+  let(:tgz_url) { 'master/cache--rvm-default--gemfile-Gemfile.tgz' }
+  subject { described_class.new() }
+
+  describe '#run' do
+    it 'calls "curl" to download .tbz archive' do
+      expect(subject).to receive(:system).with(/curl\b.*#{tbz_url}\b/m)
+      subject.run('fetch', tbz_url)
+    end
   end
 end
